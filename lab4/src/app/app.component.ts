0d70f05887d94c19b00d78a584284b91d2758b53
Mereyhaein <157210986+Mereyhaein@users.noreply.github.com>
2025-02-27 16:52:10 +0500

    Add files via upload

---
+++ lab4/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/products/header.component';
+import { NgFor } from '@angular/common';
+
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'kaspi-products';
+}
+
+
+
+
+// import {Component} from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     Hello {{ city }}, {{ 1 + 1 }}
+//   `,
+// })
+// export class AppComponent {
+//   city = 'San Francisco';
+// }
